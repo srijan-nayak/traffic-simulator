009257324511b3e54e4f2dc653f3f660628b5d65
Srijan Nayak <srijannayak0@gmail.com>
2022-09-20 10:50:29 +0530

    fix: Resize window to reasonable dimensions when starting

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -20,6 +20,7 @@ void Graphics::loadBackgroundImg() {
     // create window
     _windowName = "Concurrency Traffic Simulation";
     cv::namedWindow(_windowName, cv::WINDOW_NORMAL);
+    cv::resizeWindow(_windowName, 1892, 1000);
 
     // load image and create copy to be used for semi-transparent overlay
     cv::Mat background = cv::imread(_bgFilename);
