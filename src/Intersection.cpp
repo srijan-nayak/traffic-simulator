824de70a9ca2f1e58b1adc109a97a12941641d87
Srijan Nayak <srijannayak0@gmail.com>
2022-09-22 19:20:23 +0530

    feat: Implement L2.3

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -20,9 +20,13 @@ void WaitingVehicles::pushBack(std::shared_ptr<Vehicle> vehicle, std::promise<vo
 }
 
 void WaitingVehicles::permitEntryToFirstInQueue() {
-    // L2.3 : First, get the entries from the front of _promises and _vehicles. 
-    // Then, fulfill promise and send signal back that permission to enter has been granted.
-    // Finally, remove the front elements from both queues. 
+    auto promise = _promises.begin();
+    auto vehicle = _vehicles.begin();
+
+    promise->set_value();
+
+    _promises.erase(promise);
+    _vehicles.erase(vehicle);
 }
 
 /* Implementation of class "Intersection" */
