f097ca93ec1f65e52b52e11c743d451bdc8058bb
Srijan Nayak <srijannayak0@gmail.com>
2022-09-26 17:11:00 +0530

    feat: Implement L3.2

--- src/TrafficObject.cpp
+++ src/TrafficObject.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <chrono>
 #include <algorithm>
+#include <mutex>
 #include "TrafficObject.h"
+
+std::mutex TrafficObject::_mtxCout;
 
 // init static variable
 int TrafficObject::_idCnt = 0;
-
-// L3.2 : Add a static mutex to the base class TrafficObject (called _mtxCout) and properly instantiate it in the source file. This mutex will be used in the next task to protect standard-out. 
 
 void TrafficObject::setPosition(double x, double y) {
     _posX = x;
